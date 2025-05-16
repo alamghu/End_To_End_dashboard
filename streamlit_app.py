04b72418e120be2b449b6e225d12eff2c75ee569
alamghu <115930554+alamghu@users.noreply.github.com>
2025-05-16 11:55:10 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 alt.themes.enable("dark")
 
 # Define well names
-wells = ["Well Alpha", "Well Bravo", "Well Charlie", "Well Delta", "Well Echo", "Well Foxtrot", "Well Golf", "Well Hotel", "Well India", "Well Juliet"]
+wells = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliet"]
 
 # Define process stages
 processes = [
