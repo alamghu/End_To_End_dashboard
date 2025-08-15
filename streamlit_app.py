0c268d57de4147659450bb26726f198b0bd5b284
alamghu <115930554+alamghu@users.noreply.github.com>
2025-08-15 18:41:56 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -239,7 +239,7 @@ for well in wells:
 chart_df = pd.DataFrame(chart_data)
 if not chart_df.empty:
     fig = px.bar(chart_df, x='Process', y='Duration', color='Well', barmode='group', title='Process Visualization')
-    for process_name, kpi in kpi_days.items():
+    for process_name, kpi in kpi_days.items()
     col2.plotly_chart(fig)
 
 progress_day_df = pd.DataFrame(progress_day_data)
