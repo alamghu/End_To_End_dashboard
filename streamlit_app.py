3338b080edd4b5d5159a4fd038c5175fa9a5bec0
alamghu <115930554+alamghu@users.noreply.github.com>
2025-08-15 18:38:52 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,7 +240,7 @@ chart_df = pd.DataFrame(chart_data)
 if not chart_df.empty:
     fig = px.bar(chart_df, x='Process', y='Duration', color='Well', barmode='group', title='Process Visualization')
     for process_name, kpi in kpi_days.items():
-            fig.add_hline(y=kpi, line_dash="dot", line_color="red",
+            fig.add_hline(y=kpi, line_dash="dot", line_color="red",)
     st.plotly_chart(fig, use_container_width=True)
 
     #col2.plotly_chart(fig)
